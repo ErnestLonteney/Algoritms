ab5086183ab02c3bb38bfbd89180b653d7beecb2
Nesterchuk Volodymyr <ampersant@ukr.net>
2026-02-09 21:12:30 +0100

    Added array operations

---
+++ Arrays/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace Arrays
+{
+    internal class Program
+    {
+        static int FindMax(int[] arr)
+        {
+            int max = arr[0];
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+            }
+
+            return max;
+        }
+
+        static int FindMin(int[] arr) // O(n-1)
+        {
+            int min = arr[0];
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+            }
+
+            return min;
+        }
+
+        static int CalculateAverage(int[] arr) // O(n + 1)
+        {
+            int sum = SumOfNumbers(arr);
+
+            return sum / arr.Length;
+        }
+
+        static int CountEvenNumber(int[] arr) // O(n)
+        {
+            int count = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] % 2 == 0)
+                    count++;
+            }
+
+            return count;
+        }
+
+        static int SumOfNumbers(int[] arr) // O(n)
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+
+            return sum;
+        }
+
+        static void Main()
+        {
+            int[] numbers = [12, 45, 0, -56, 23, 17, 70];
+
+            var minElement = FindMin(numbers);
+            var maxElement = FindMax(numbers);
+            var sum = SumOfNumbers(numbers);
+            var count = CountEvenNumber(numbers);
+
+            Console.WriteLine($"Min number is {minElement}");
+            Console.WriteLine($"Max number is {maxElement}");
+            Console.WriteLine($"Sum of elements is {sum}");
+            Console.WriteLine($"Count of positive numbers is {count}");
+        }
+    }
+}
---
+++ QuickSort/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace QuickSort
+{
+    internal class Program
+    {
+        static int[] QuickSort(int[] array)
+        {
+            if (array.Length < 2)
+                return array;
+
+            var pivot = array[0];
+            var rest = array[1..];
+
+            var ar1 = rest.Where(x => x < pivot).ToArray();
+            var ar2 = rest.Where(x => x > pivot).ToArray();
+
+            return [..QuickSort(ar1), pivot, ..QuickSort(ar2)];
+        }
+
+        static int[] QuickSortOptimized(int[] array, int leftIndex, int rightIndex)
+        {
+            if (array.Length < 2)
+                return array;
+
+            var pivotIndex = leftIndex;
+
+            while (pivotIndex < rightIndex)
+            {
+                if (array[pivotIndex] > array[rightIndex])
+                {
+                    var buf = array[pivotIndex];
+                    array[pivotIndex] = array[rightIndex];
+                    array[rightIndex] = buf;
+                    pivotIndex = rightIndex;
+                }
+                else
+                {
+                    rightIndex--;
+                }
+            }          
+
+            return [..QuickSortOptimized(ar1, leftIndex, rightIndex), array[pivotIndex], ..QuickSortOptimized(ar2, leftIndex, rightIndex)];
+        }
+
+        static void Main()
+        {
+            int[] array = [ 5, 2, 9, 1, 5, 6 ];
+
+            var res = QuickSort(array);
+
+            Console.WriteLine(string.Join(", ", res));
+        }
+    }
+}
