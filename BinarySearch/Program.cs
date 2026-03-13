bcd95eb5f9b4b49b1941fcd06a293d173164676d
Volodymyr <volodymyr@mail.com>
2026-03-13 14:27:56 +0100

    Added upper text

--- BinarySearch/Program.cs
+++ BinarySearch/Program.cs
@@ -1,46 +1,27 @@
-﻿namespace BinarySearch
+﻿
+using BinarySearch.Extensions;
+
+namespace BinarySearch
 {
     internal class Program
     {
-        static int SimpleSearch(int[] arr, int target) // O(n)
-        {
-            for (int i = 0; i < arr.Length; i++)
-            {
-                if (arr[i] == target)
-                    return i;
-            }
-
-            return -1;
-        }
-
-        static int BinarySearch(int[] arr, int target)    // O(log n)
-        {
-            int leftIndex = 0;
-            int rightIndex = arr.Length - 1;
-
-            while (leftIndex <= rightIndex)
-            {
-                int middleIndex = (leftIndex + rightIndex) / 2;
-
-                if (arr[middleIndex] == target)
-                   return middleIndex;
-                else 
-                    if (target > arr[middleIndex] )
-                    leftIndex = middleIndex + 1;
-                else
-                    rightIndex = middleIndex - 1;
-            }
-
-            return -1;
-        }
-
         static void Main(string[] args)
         {
-            int[] numbers = [1, 10, 13, 77, 89, 100, 200, 300, 400, 500, 600, 1000]; 
 
-            var res = BinarySearch(numbers, 300);
+            int[] numbers = [1, 10, 13, 77, 89, 100, 200, 300, 400, 500, 600, 1000];
 
-            Console.WriteLine(res);
+            var res = numbers.BinarySearch(300);
+
+            int[] newArray = new int[10];
+            newArray.SimpleSearch(5);
+
+
+            int n = 10;
+            n.DisplayNumber();
+
+
+            int m = 20;
+            m.DisplayNumber();
         }
     }
 }
